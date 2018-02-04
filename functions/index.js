f6afc560dc7e9b15c84e0a6df9a61cde2779cbea
Mayank Kumar <mayankkumar@urbanclap.com>
2018-02-04 10:03:10 +0530

    comment out server side functions

--- functions/index.js
+++ functions/index.js
@@ -34,26 +34,27 @@ function getTextMessage(clientText) {
   });
 }
 
-// Sends a notifications to all users when a new message is posted.
+/* Sends a notifications to all users when a new message is posted.
 exports.sendServerChatMessage = functions.database.ref('/messages/{messageId}').onCreate((event) => {
-  // const snapshot = event.data;
-  // const data = snapshot.val();
-  // const clientText = data.text || '';
-  // console.log('TEXT>>>>', data);
+  const snapshot = event.data;
+  const data = snapshot.val();
+  const clientText = data.text || '';
+  console.log('TEXT>>>>', data);
 
-  // if (data.type === 'client') {
-  //   return getTextMessage(clientText)
-  //     .then(function(text) {
-  //       return admin.database().ref('messages').push({
-  //         name: 'Server',
-  //         type: 'server',
-  //         photoUrl: 'http://www.newdesignfile.com/postpic/2012/11/minecraft-factions-server-icons_177429.png',
-  //         text: text
-  //       });
-  //     })
-  //     .catch(function(text) {
-  //       return null
-  //     });
-  // }
-  // return null;
+  if (data.type === 'client') {
+    return getTextMessage(clientText)
+      .then(function(text) {
+        return admin.database().ref('messages').push({
+          name: 'Server',
+          type: 'server',
+          photoUrl: 'http://www.newdesignfile.com/postpic/2012/11/minecraft-factions-server-icons_177429.png',
+          text: text
+        });
+      })
+      .catch(function(text) {
+        return null
+      });
+  }
+  return null;
 });
+*/
